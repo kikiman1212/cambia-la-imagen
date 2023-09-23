14fd9c6c1646aa8cbb5e5ca2879c174638fd6c5f
FRANCISCO KIKO <107635035+kikiman1212@users.noreply.github.com>
2023-09-23 14:46:22 -0600

    Add files via upload

--- min.js
+++ min.js
@@ -18,23 +18,38 @@ function cambiarImagen(){
 	if(edad.value >= 18 && sexo.value == "Masculino"){
 	document.getElementById('imgCambia').src = img5;
         Nombre.value = ""; edad.value = "";
+        esconderImagen()
 	 }
 	else if(edad.value > 4 && sexo.value == "Femenino"){
 	document.getElementById('imgCambia').src = img6;
         Nombre.value = ""; edad.value = "";
+         esconderImagen()
 	 }
         else if(Nombre.value == "ANGEL" && edad.value == 8 && sexo.value == "Masculino"){
 	document.getElementById('imgCambia').src = img3;
          alert('Hola Angel Gustavo tienes 8 Años, y eres cinta verde avanzada 🥋🏆, y te gusta ver Raimo Friends');
-	 }
+	  Nombre.value = ""; edad.value = "";	
+          esconderImagen()
+         }
 	else if(edad.value < 18 && sexo.value == "Masculino"){
 	document.getElementById('imgCambia').src = img1;
         Nombre.value = ""; edad.value = "";
+         esconderImagen()
 	}else{
 	alert('Introduce tu Nombre, Edad y Sexo 😎👨‍💻❌');
 }	
 }
 
+function regresarCaja(){
+        document.getElementById("esconder").style.display = "flex";
+      
+}
+
+function esconderImagen(){
+        document.getElementById("esconder").style.display = "none";
+        
+
+}
 
 
 
